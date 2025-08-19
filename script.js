f9c03d4b888f4f44d1a1be93741f8fbf4ae23c40
Aditya Vishwakarma <144864627+Dev-adityavishwa@users.noreply.github.com>
2025-08-19 15:32:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// document.body;
+
+
+const box11 = document.getElementById('box1')
+box11.addEventListener('click', () => {
+    box11.classList.add('bg1');
+});
+const box21 = document.getElementById('box2')
+box21.addEventListener('click', () => {
+    box21.classList.add('bg2');
+});
+const box31 = document.getElementById('box3')
+box31.addEventListener('click', () => {
+    box31.classList.add('bg3');
+});
+const box41 = document.getElementById('box4')
+box41.addEventListener('click', () => {
+    box41.classList.add('bg4');
+});
+
+
+
+
+function changeGreeting() {
+      const name = document.getElementById("nameInput").value;
+      const greeting = document.getElementById("greeting");
+
+      if (name.trim() !== "") {
+        greeting.textContent = "Hello, " + name;
+      } else {
+        greeting.textContent = "Hello, Guest";
+      }
+    }
+
+
+
+
+
+
